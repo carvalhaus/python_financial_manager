795ac4b7c9a39bdf9f3a10481df9d3a711e6fe5b
Joao Pedro Carvalho <ooliveira.joaop@gmail.com>
2025-02-14 19:26:36 -0300

    feat: create transaction_manager class

--- finance_manager/transactions.py
+++
@@ -1,7 +0,0 @@
-class Transaction:
-    def __init__(self, amount, category):
-        self.amount = amount
-        self.category = category
-
-    def __str__(self):
-        return f"R$ {self.amount:.2f} in {self.category}"
